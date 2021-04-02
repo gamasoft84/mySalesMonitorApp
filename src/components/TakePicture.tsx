2708dd2c74a165649ecb3dee2ef00b688dee10bc
Richard Gama <rgamag128@gmail.com>
2021-04-01 19:10:28 -0600

    add IonMenuButton

--- src/components/TakePicture.tsx
+++ src/components/TakePicture.tsx
@@ -8,51 +8,48 @@ import {
   IonFab,
   IonFabButton,
   IonIcon,
+  IonButtons,
+  IonMenuButton,
 } from "@ionic/react";
 import { camera } from "ionicons/icons";
-import { Plugins, CameraResultType } from '@capacitor/core';
+import { Plugins, CameraResultType } from "@capacitor/core";
 const { Camera } = Plugins;
 
 const TakePicture: React.FC = () => {
+  const [image, setImage] = useState<string>("");
 
-const [image, setImage] = useState<string>('');
-
-
-const takePicture = async () => {
+  const takePicture = async () => {
     const photo = await Camera.getPhoto({
-        quality: 90,
-        allowEditing: true,
-        resultType: CameraResultType.Uri
-      });
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
 
-      setImage(photo.webPath || '');
-
-}
-
+    setImage(photo.webPath || "");
+  };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
           <IonTitle>Take Image</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent>
+        {image != "" ? <img src={image}/> : <p>Take a Picture</p>}
 
-    { image != ''
-        ? <img src={image}/>
-        : <p>Take a Picture</p>
-    }
-
-      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePicture()}>
-              <IonIcon icon={camera}></IonIcon>
+            <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
-      </IonFab>
-      </IonContent>      
+        </IonFab>
+      </IonContent>
     </IonPage>
   );
 };
-
 
 export default TakePicture;
